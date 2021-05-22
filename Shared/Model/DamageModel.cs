160705537f1a90ccaf007fb34148c26c65c27a78
Stefan Lochau <lochaus@hotmail.com>
2021-05-22 21:57:02 +0200

    Added DamageModel.cs

---
+++ Shared/Model/DamageModel.cs
@@ -0,0 +1,10 @@
+namespace Ps2TtkCalculator
+{
+  public class DamageModel
+  {
+    public int MaxDamage {get;set;}
+    public int MinDamage { get; set; }
+    public int MaxDamageRange_m { get; set; }
+    public int MinDamageRange_m { get; set; }
+  }
+}
