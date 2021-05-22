e5fff331dc61c619c312c9e959485c736cb0a9e4
Stefan Lochau <lochaus@hotmail.com>
2021-05-22 22:01:24 +0200

    Fixed formatting

--- Shared/Model/DamageModel.cs
+++ Shared/Model/DamageModel.cs
@@ -2,7 +2,7 @@ namespace Ps2TtkCalculator
 {
   public class DamageModel
   {
-    public int MaxDamage {get;set;}
+    public int MaxDamage { get; set; }
     public int MinDamage { get; set; }
     public int MaxDamageRange_m { get; set; }
     public int MinDamageRange_m { get; set; }
