71ff3187fa100ed553fd94d662ad382cac3d7044
Stefan Lochau <lochaus@hotmail.com>
2021-05-23 13:46:43 +0200

    Implemented Target

--- Shared/Model/Target.cs
+++ Shared/Model/Target.cs
@@ -2,6 +2,8 @@ namespace Ps2TtkCalculator
 {
     public class Target
     {
-
+        public int MaxHP { get; set; }
+        public double ResistanceHeadshot { get; set; }
+        public double ResistanceBodyshot { get; set; }
     }
 }
