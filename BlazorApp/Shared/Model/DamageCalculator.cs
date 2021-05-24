f054b587934c2c0564a4aaf9bff647dd7a90aed5
Stefan Lochau <lochaus@hotmail.com>
2021-05-24 13:17:06 +0200

    Added first test and fixed bug in damage calculation

--- BlazorApp/Shared/Model/DamageCalculator.cs
+++ BlazorApp/Shared/Model/DamageCalculator.cs
@@ -4,27 +4,23 @@ namespace Ps2TtkCalculator
     {
         public static int DamagePerBodyShot(Weapon weapon, Target target, int range_m)
         {
-            double RawDamage = weapon.GetRawDamageAt(range_m);
+            double RawDamage = GetRawDamageAt(weapon, range_m);
             return (int)(RawDamage * (1 - target.ResistanceBodyshot));
         }
 
         public static int DamagePerHeadShot(Weapon weapon, Target target, int range_m)
         {
-            double RawDamage = weapon.GetRawDamageAt(range_m);
+            double RawDamage = GetRawDamageAt(weapon, range_m);
             return (int)(RawDamage * weapon.HeadshotMultiplier * (1 - target.ResistanceHeadshot));
         }
 
-        private static double GetRawDamageAt(this Weapon weapon, int range_m)
+        private static double GetRawDamageAt(Weapon weapon, int range_m)
         {
-            if (weapon.DamageModel.MaxDamageRange_m < range_m)
+            if (range_m < weapon.DamageModel.MaxDamageRange_m)
             {
                 return weapon.DamageModel.MaxDamage;
             }
             else if (range_m < weapon.DamageModel.MinDamageRange_m)
-            {
-                return weapon.DamageModel.MinDamage;
-            }
-            else
             {
                 // range = lambda * MaxRange + (1- lambda) * MinRange =>
                 var lambda = (double)(range_m - weapon.DamageModel.MinDamageRange_m)
@@ -32,7 +28,10 @@ namespace Ps2TtkCalculator
                 return lambda * weapon.DamageModel.MaxDamage
                        + (1 - lambda) * weapon.DamageModel.MinDamage;
             }
-
+            else
+            {
+                return weapon.DamageModel.MinDamage;
+            }
         }
     }
 }
