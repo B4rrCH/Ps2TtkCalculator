12eb608b281667f749e459c11025e1d6a18fc3b7
Stefan Lochau <lochaus@hotmail.com>
2021-06-13 18:59:00 +0200

    Moved @using MudBlazor to _Imports.razor and removed @using Radzen(.Blazor)

--- BlazorApp/Shared/Views/WeaponPreview.cs
+++ BlazorApp/Shared/Views/WeaponPreview.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Ps2TtkCalculator.Shared.Model;
 using Ps2TtkCalculator.Shared.Dto;
 using Ps2TtkCalculator.Shared.ExtensionMethods;
