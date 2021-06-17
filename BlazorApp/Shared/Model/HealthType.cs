2c5f540ab054cc96f1f6830ce479cc5ba4d264a1
Stefan Lochau <lochaus@hotmail.com>
2021-06-17 20:23:51 +0200

    Moved HealthType and ResistanceType to model

---
+++ BlazorApp/Shared/Model/HealthType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Ps2TtkCalculator.Shared.Model
+{
+    enum HealthType
+    {
+        [Description("Normal (1000)")]
+        Normal,
+        [Description("NMG/Adren Shield (1425)")]
+        NanomeshGenerator,
+        [Description("Infiltrator (900)")]
+        Infiltrator
+    }
+}
---
+++ BlazorApp/Shared/Model/ResistanceType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Ps2TtkCalculator.Shared.Model
+{
+    enum ResistanceType
+    {
+        [Description("Normal (0%)")]
+        Normal,
+        [Description("Nanoweave Armor (20% body-only)")]
+        NanoWeaveArmor,
+        [Description("Symbiote (20%)")]
+        Symbiote,
+        [Description("ResistShield (35%)")]
+        ReistShield
+    }
+}
